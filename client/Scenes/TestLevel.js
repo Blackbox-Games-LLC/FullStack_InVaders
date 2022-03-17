d41caf8f5c4f5bc33dbb38578e59288bef63402a
David <davruiz07@gmail.com>
2022-03-17 14:54:46 -0400

    next init

---
+++ client/Scenes/TestLevel.js
@@ -0,0 +1,10 @@
+import Phaser from 'phaser'
+
+export default class Test extends Phaser.Scene {
+    constructor() {
+        super('Test')
+    }
+    preload() { }
+    create() { }
+    update() { }
+}
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import Phaser from 'phaser'
+import Test from '../Scenes/TestLevel';
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 760,
+    physics: {
+        arcade: {
+            debug: true,
+            gravity: { scale: 0 }
+        },
+        matter: {
+            debug: true,
+            gravity: { scale: 0 }
+        }
+    },
+};
+
+const game = new Phaser.Game(config)
+
+game.scene.add('Test_Level', Test)
+game.scene.start('Test_Level')
 
 const Home = () => {
-    return (
-        <h1>Hello World</h1>
-    )
+    return (<div></div>)
 }
 
 export default Home
