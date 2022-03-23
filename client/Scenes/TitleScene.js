240902c49e9823529345757f4084f7a20c7f1361
veysel basbaydar <vgbasbaydar@gmail.com>
2022-03-23 08:17:43 -0700

    acces to login page

--- client/Scenes/TitleScene.js
+++ client/Scenes/TitleScene.js
@@ -8,29 +8,17 @@ export default class TitleScene extends Phaser.Scene {
 
   preload() {
     //   this.load.image('background','assets/title.png')
-    // this.load.html("form", "../assets/text/form.html");
     this.load.html("form", "/assets/text/form.html");
   }
 
   create() {
-    // console.log(TitleScene);
-    var bg = this.add.sprite(0, 0, "background");
-    bg.setOrigin(0, 0);
-    var text = this.add.text(
-      500,
-      500,
-      "FULLSTACK INVADERS. CLICK HERE TO START"
-    );
-    text.setInteractive({ userHandCursor: true });
-    text.on("pointerdown", () => this.clickButton());
-
     this.nameInput = this.add.dom(2000, 1500).createFromCache("form");
     this.nameInput.width = 400;
     this.nameInput.displayWidth = 400;
     this.nameInput.height = 250;
     this.nameInput.displayHeight = 250;
 
-    console.log("input", this.nameInput);
+    //console.log("inputname", this.nameInput);
     this.message = this.add
       .text(2000, 1000, "Hello Please Login", {
         color: "#FFFFFF",
@@ -43,20 +31,22 @@ export default class TitleScene extends Phaser.Scene {
     this.returnKey = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.ENTER
     );
+    // console.log("return key", this.returnKey);
 
     this.returnKey.on("down", async (event) => {
       let name = this.nameInput.getChildByName("name");
+      // console.log("name", name);
       let loginResult = await axios.post("/api/login", {
         username: name.value,
       });
       console.log("loginResult", loginResult);
-      if (!loginResult) {
-        this.text.setText("Please check your UserName");
-        return false;
+      if (loginResult.data.username === name.value) {
+        this.scene.switch("Test_Level");
+      } else {
+        this.message.setText("Wrong User Name");
       }
       name.value = "";
     });
-    //this.scene.switch("Test_Level");
   }
   clickButton() {
     this.scene.switch("Test_Level");
